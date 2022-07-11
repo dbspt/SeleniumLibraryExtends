3da5e50bec3ede2f9bd47c3b32e09614cf2d94bd
Cleverson Sampaio <cleverson@sampaio.dev.br>
2022-07-11 16:24:49 -0300

    feat(findelements): added element wait
    added wait for element to appear or disappear

---
+++ src/SeleniumLibraryExtends/FindElements/findElements.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Cléverson Sampaio <cleverson@sampaio.dev.br>
+
+from robot.libraries.BuiltIn import BuiltIn
+from robot.api.deco import keyword
+from SeleniumLibraryExtends.report import Report
+
+class FindElements:
+    def __init__(self):
+        pass
+    
+    @keyword("I wait for the ${locator} element to be visible")
+    def waitForElementVisible(self, locator: str):
+        self.driver = BuiltIn().get_library_instance("SeleniumLibrary")
+        try:
+            self.driver.wait_until_element_is_visible(locator)
+        except Exception as e:
+            Report().fail(e)
+
+    @keyword("I wait for the ${locator} element not to be visible")
+    def waitForElementNotVisible(self, locator: str):
+        self.driver = BuiltIn().get_library_instance("SeleniumLibrary")
+        try:
+            self.driver.wait_until_element_is_not_visible(locator)
+        except Exception as e:
+            Report().fail(e)
